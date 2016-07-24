2470e6eb32bfc01dc9f5221cc66b49617f954506
Velenir <Velenir@users.noreply.github.com>
2016-07-24 20:18:50 +0300

    Added GET request for the channel if a stream is offline

--- js/getstreams.js
+++ js/getstreams.js
@@ -2,9 +2,10 @@ const defaultChannels = ["freecodecamp", "storbeck", "terakilobyte", "habathcx",
 
 const $streams = $(".streams");
 
-function getStreamItem(channelName, game, logo, url) {
+function constructStreamItem({display_name, game, logo, url}) {
+	// console.log("CONSTRUCTING ITEM FROM", {display_name, game, logo, url});
 	const $item = $(`<a class='collection-item stream-item row center-align'>
-										<p class='channel-name col s12 m2 l2'>${channelName}</p>
+										<p class='channel-name col s12 m2 l2'>${display_name}</p>
 									</a>`);
 
 	if(url) {
@@ -18,7 +19,7 @@ function getStreamItem(channelName, game, logo, url) {
 	if(logo) {
 		const img = new Image();
 		// img.className = "thumbnail col s12 m2 l1";
-		img.alt = channelName;
+		img.alt = display_name;
 		promise = new Promise(function (resolve) {
 			// resolve only, don't reject anything
 			img.onload = resolve;
@@ -37,55 +38,66 @@ function getStreamItem(channelName, game, logo, url) {
 	return Promise.resolve(promise).then(() => $item);
 }
 
-function processStreamData({stream}, channelName) {
-	console.log("stream:", stream);
-	let game, name, logo, url;
-
-	// Online
-	if(stream) {
-		({game, channel: {name, logo, url}} = stream);
-		console.log("game:", game);
-		console.log("channel name:", name);
-		console.log("channel logo:", logo);
-	} else {
-		name = channelName;
-		game = "Offline";
-		url = "https://www.twitch.tv/" + name;
-	}
-
-	getStreamItem(channelName, game, logo, url).then($item => {
-		if(!stream) $item.addClass("offline");
-		$streams.prepend($item);
+function getStream(channelName) {
+	// console.log("GETTING STREAM");
+	return $.ajax({
+		url: 'https://api.twitch.tv/kraken/streams/' + channelName,
+		type: 'GET',
+		dataType: 'json',
+		headers: {Accept : 'application/vnd.twitchtv.v3+json'}
 	});
 }
 
-function getChannels(channels) {
+function getChannel(channelName) {
+	// console.log("GETTING CHANNEL");
+	return $.ajax({
+		url: 'https://api.twitch.tv/kraken/channels/' + channelName,
+		type: 'GET',
+		dataType: 'json',
+		headers: {Accept : 'application/vnd.twitchtv.v3+json'}
+	});
+}
+
+function getStreams(channels) {
 	for(let channel of channels) {
-		$.ajax({
-			url: 'https://api.twitch.tv/kraken/streams/' + channel,
-			type: 'GET',
-			dataType: 'json',
-			headers: {Accept : 'application/vnd.twitchtv.v3+json'}
-		})
-		.done(function(data) {
-			// console.log("success");
-			// console.log(data);
-			processStreamData(data, channel);
-		})
-		.fail(function(jqXHR) {
-			if(jqXHR.status === 422) {
-				console.log("Channel Unavailable");
-				getStreamItem(channel, "Unavailable").then($item => $streams.prepend($item.addClass("unavailable")));
+		// console.log("*=========*");
+		getStream(channel).then(data => {
+			// console.log("1. GOT STREAM:", data.stream);
+			if(data.stream) {
+				const {game, channel: {display_name, logo, url}} = data.stream;
+				// return constructStreamItem(display_name, game, logo, url);
+				return {game, display_name, logo, url};
 			} else {
-				console.log("ERROR. jqXHR:",jqXHR);
+				return getChannel(channel).then(data => {
+					// console.log("1.5 GOT CHANNEL:", data);
+					const {display_name, logo, url} = data;
+					// return constructStreamItem(display_name, "Offline", logo, url).then($item => $item.addClass("offline"));
+					return {game: "Offline", display_name, logo, url};
+				});
 			}
-		})
-		.always(function() {
-			// console.log("complete");
-		});
+		}).catch((jqXHR, status, err) => {
+			// console.log("2X Failure", jqXHR, status, err);
+			if(jqXHR.status === 422) {
+				// console.log("Channel Unavailable");
+				// constructStreamItem(channel, "Unavailable").then($item => $streams.prepend($item.addClass("unavailable")));
+				return {game: "Unavailable", display_name: channel};
+			} else {
+				// console.log("Error. jqXHR:",jqXHR);
+				throw new Error(status);
+			}
+		}).then(streamStats => {
+			// console.log("2 GOT STREAM STATS:", streamStats);
+			return constructStreamItem(streamStats).then($item => {
+				if(streamStats.game === "Offline" || streamStats.game === "Unavailable") {
+					$item.addClass(streamStats.game.toLowerCase()).appendTo($streams);
+				} else $streams.prepend($item);
+				// console.log("2.5 FINAL ITEM:", $item[0]);
+			});
+		}).catch(err => console.log("Error:", err));
 	}
 }
 
+
 $(document).ready(function() {
-	getChannels(defaultChannels);
+	getStreams(defaultChannels);
 });
